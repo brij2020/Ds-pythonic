ec48d7da2073311443d28ba9932bb22a401b1f7e
Brijbhan <brijphy@gmail.com>
2018-07-24 08:29:02 +0530

    rewrite create list function

--- Doubly_linked_list/Doubly_Linked_List.py
+++ Doubly_linked_list/Doubly_Linked_List.py
@@ -6,72 +6,73 @@
 #                                             #           
 ###############################################
 
-'''   class for creating node with two refrences having next and prev '''  
+
 class Node(object):
+    next = None
     prev = None
     data = None
-    next = None
     pass
 
-class Doubly_Linked_List(object):
-    # two refrences for traversion list from both ends
-    current_node = None
-    tail         = None
+class Doubly_List(object):
+    head = None
+    tail = None
 
-    ''' function for creating doubly linked list  '''
-    def createList(self, val):
+    def add_node_empty_list(self, val):
         node = Node()
         node.data = val
-        node.next = self.current_node
-        self.current_node = node 
-        if self.current_node.next != None:
-            node.next.prev = node
-        if self.current_node.next == None:
-            self.tail = self.current_node
-            
+        node.next = self.head
+        self.head = node
+        if self.head.next == None :
+            self.tail = self.head
+        
+        if self.head.next != None :
+            self.head.next.prev = self.head
+
         pass
 
-    ''' function for traverse list '''
-    def printFromCurrentNode(self):
-        node = self.current_node
+    def print_list_from_head(self):
+        node = self.head
         while node :
             print(node.data)
             node = node.next
-
-    ''' function for traverse list form tail '''
-    def PrintFromTail(self):
+        pass
+    
+    def print_list_from_tail(self):
         node = self.tail
-        while node :
+        while node:
             print(node.data)
-            node  = node.prev
+            node = node.prev
+        
+        pass
 
-    ''' function for size of the list '''
-    def listSize(self):
-        node  = self.current_node
-        i = 0
+
+    def list_size(self):
+        node = self.head
+        i = 0 
         while node :
             i+=1
             node = node.next
-        return i
+        return i    
 
-    ''' function for adding new node at begning to list O(1) '''
-    def add_node_at_begning(self,val):
-        node = Node()
-        node.data = val
-        node.next = self.current_node
-        self.current_node = node
-        if self.current_node.next != None:
-            self.current_node.next.prev = self.current_node
 
-        pass    
+
 
 def main():
-    
-    li = Doubly_Linked_List()
-    for name in ['Ruby','C','C#','Java','Pascal','PHP']:
-        li.createList(name)
+    li = Doubly_List()
 
-   
+    li.add_node_empty_list(12)
+    li.add_node_empty_list('Ravi')
+    li.add_node_empty_list('Jack')
+    for name in ['ramesh','jaiya','beef','suman','ravi','soni','saumya']:
+        li.add_node_empty_list(name)
+    li.print_list_from_head()
+    print('\n----------second operation ------\n')
+    li.print_list_from_tail()
+    print('\n------------check------------\n')
+    print(li.list_size())
+    pass
 
-if  __name__ == '__main__':
+
+
+if __name__ == '__main__':
     main()
