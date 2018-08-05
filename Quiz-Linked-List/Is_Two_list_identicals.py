c7b0106dca746beba42c31fbaaa4a426f076aff9
Brijbhan <brijphy@gmail.com>
2018-08-05 11:56:17 +0530

    identical function

--- Quiz-Linked-List/Is_Two_list_identicals.py
+++ Quiz-Linked-List/Is_Two_list_identicals.py
@@ -68,17 +68,12 @@ def main():
     li2.create_list('govi')
     li2.create_list('ravi')
     li2.create_list('jai')
+    
     x = li1.isIdentical(li2)
         
     print(x)
     
     pass
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
