4ec575e028abd175ef53ecf864c023396b3baf4b
Brijbhan <brijphy@gmail.com>
2018-08-11 20:57:12 +0530

    lack

---
+++ Queue/Queue.py
@@ -0,0 +1,24 @@
+#################################################
+#                                               #    
+#                                               #
+#                                               #            
+#                                               #            
+#           QUEUE IMPLIMENTATIONS               #
+#  (LIFO ADT base data structure Linked List)   #                                #        
+#                                               #            
+#                                               #    
+#                                               #            
+#################################################
+""" 
+   
+        methods to applied 
+        1-  insert()
+        2-  del()
+        3-  peek()
+        4-  display()
+        5-  isEmpty()
+
+"""
+
+
+
