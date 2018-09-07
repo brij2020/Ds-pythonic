0d7673b6534f7f512ad1f78983991eb2e5ee401d
Brijbhan <brijphy@gmail.com>
2018-09-07 09:25:40 +0530

    qiuze: split list into even node and node

--- Quiz-Linked-List/Split_list_in_part_alternate_even_nodes_in_second_list.py
+++ Quiz-Linked-List/Split_list_in_part_alternate_even_nodes_in_second_list.py
@@ -24,10 +24,27 @@ class List(object):
         node.data = val
         node.next = self.head
         self.head = node
-
     pass
 
-    
+    ''' method for split list at alternate nodes'''
+    def split_node_alternate(self):
+        node = self.head
+        prev = None
+        link_node = None
+        counter = 0
+        even_list = List()
+        while node :
+            prev = node
+            counter += 1
+            if counter % 2 == 0:
+                link_node.next = prev.next
+                even_list.createList(prev.data)
+            else :
+                link_node = node
+            node = node.next
+        return even_list
+
+
     ''' for print list '''
     def print_list(self):
         node = self.head
@@ -39,15 +56,17 @@ class List(object):
 
 def main():
     lis = List()
-    for name in ['ram', 'shyam', 'jaya', 'shushma']:
+    for name in [ x for x in range(0,10)]:
         lis.createList(name)
-    
-    
-    
-    
-    
-    
-    print('\n namespace')
+    print('----original list ----\n')
+    lis.print_list()
+
+    newl = lis.split_node_alternate()
+    print('\n----- even List --- \n')
+    newl.print_list()
+    print('\n----odd list---- ')
+    lis.print_list()
+    print('\n-----namespace ------\n')
     print(dir())
 
     print('hello, world')
